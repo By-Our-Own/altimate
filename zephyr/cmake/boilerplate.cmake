3293dc8f3dc291ff7b207a435e9adfe452dcac4c
Ioannis Konstantelias <ikonstadel@gmail.com>
2020-03-22 13:33:19 +0200

    zephyr: samples: Add sample with BMP388 & LPS22HH
    Added BMP388 driver, added cmake boilerplate files and zephyr
    tree based project structure. Also updated the west definition.
    
    Signed-off-by: Ioannis Konstantelias <ikonstadel@gmail.com>

---
+++ zephyr/cmake/boilerplate.cmake
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2020 Ioannis Konstantelias
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set(BOO_DIR ${CMAKE_CURRENT_LIST_DIR}/..)
+
+message(${BOO_DIR})
+
+list(APPEND DTS_ROOT ${BOO_DIR})
